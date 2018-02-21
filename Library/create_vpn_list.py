d1b0d14a3e372359e6760de4eaa957649bb20d4c
Chris Hocker <chocker@cisco.com>
2018-02-21 13:40:12 -0500

    Added ansible.cfg and hosts file

--- Library/create_vpn_list.py
+++ Library/create_vpn_list.py
@@ -3,12 +3,11 @@
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.viptela import rest_api_lib
 import requests
-import sys
-import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def main():
+    # Define Ansible module argument spec
     module_args = {
         "vmanage_ip": {"required": True, "type": "str"},
         "username": {"required": True, "type": "str"},
@@ -23,8 +22,10 @@ def main():
         }
     }
 
+    # Instantiate Ansible module object
     module = AnsibleModule(argument_spec=module_args)
 
+    # Load all the Ansible parameters into local variables\
     vmanage_ip = module.params['vmanage_ip']
     username = module.params['username']
     password = module.params['password']
@@ -33,30 +34,33 @@ def main():
     entries = []
     for vpn in module.params['vpns']:
         entries.append({"vpn": str(vpn)})
+    # Create requests playload for Post and Put methods
     payload = {
         "name": name,
         "description": description,
         "entries": entries
     }
 
+    # Instantiate vmanage requests session
     obj = rest_api_lib(vmanage_ip, username, password)
+    # Check if VPN list already exists and get VPN list entries
     listId, current_entries = obj.get_vpn_list_by_name(module.params['name'])
 
+    # If the VPN list does not exist, create it via post
     if not listId:
-        response = obj.post_request('template/policy/list/vpn', payload=payload)
-    else:
-        if obj.compare_vpn_list_entries(current_entries, entries):
+        response = obj.post_request('template/policy/list/vpn', payload=payload)  
+        if response.status_code == 200:
+            listId = response.json()['listId']
+            module.exit_json(changed=True, listId=listId)
+        else:
+            module.fail_json(msg="Error", listId=listId)
+    # If the VPN list does exist, compare the VPN entries, and if necessary, update via post        
+    if listId:
+        if current_entries == entries:
             module.exit_json(changed=False, msg="No changes needed", listId=listId)
         else:
             obj.put_request('template/policy/list/vpn/' + listId, payload=payload)
-            module.exit_json(changed=True, msg="Updating Entries", listId=listId)
-
-    if response.status_code == 200:
-        #listId = response.json()['listId']
-        module.exit_json(changed=True)
-    else:
-        module.fail_json(msg="Error", listId=listId)
-
+            module.exit_json(changed=True, msg="Updating list", listId=listId)
 
 if __name__ == '__main__':
     main()
